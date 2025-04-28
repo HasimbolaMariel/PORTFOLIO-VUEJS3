98ab7c8facf2a2733e89d1e19c7f5dc26175a8fc
HasimbolaMariel <randrianasoloniainamariel@gmail.com>
2025-04-28 15:02:12 +0300

    reglage image sur mobile

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -6,12 +6,13 @@
     >
       <!-- Fond uni (ou image plus tard) -->
       <div :class="[ 'h-60 w-full']">
-        <img :src="project.image" alt="" class="h-full">
+        <img :src="project.image" alt="" class="h-full w-full">
       </div>
   
       <!-- Hover overlay -->
       <div
-        class="absolute inset-0 bg-black/70 flex flex-col items-center justify-center text-center p-6 opacity-0 group-hover:opacity-100 transition duration-500 ease-in-out"
+        class="absolute inset-0 bg-black/70 flex flex-col items-center justify-center text-center p-6 opacity-0 group-hover:opacity-100 transition duration-500 ease-in-out" 
+        :class="{'opacity-100': isMobile, 'opacity-0 group-hover:opacity-100': !isMobile}"
       >
         <h3 class="text-white font-bold text-2xl mb-2">{{ project.title }}</h3>
         <p class="text-gray-300 text-sm mb-4">{{ project.description }}</p>
@@ -36,6 +37,8 @@
   </template>
   
   <script setup>
+  const isMobile = window.innerWidth <= 768
+
   defineProps({
     project: Object
   })
